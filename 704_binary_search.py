0e217b25ce47da529718947a675e3e25e93631a4
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-04-29 15:19:50 +0600

    algo_day_1__binary_search

---
+++ 704_binary_search.py
@@ -0,0 +1,44 @@
+# Time complexity: O(logN)
+# Space complexity: O(1)
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return -1
+
+
+
+
+
+# recursion
+# Time complexity: O(logN)
+# Space complexity: O(logN)
+
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+        
+#         return self.recursive_search(nums, 0, len(nums) - 1, target)
+    
+#     def recursive_search(self, nums, left, right, target):
+#         if left > right:
+#             return -1
+        
+#         mid = (left + right) // 2
+#         if target < nums[mid]:
+#             return self.recursive_search(nums, left, mid - 1, target)
+        
+#         if target > nums[mid]:
+#             return self.recursive_search(nums, mid + 1, right, target)
+        
+#         return mid
