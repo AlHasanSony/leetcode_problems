59d21ccad5566c255aa17f473d22bf0e297988f6
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-05-08 08:17:44 +0600

    alg day 1

---
+++ search_insert_position.py
@@ -0,0 +1,16 @@
+#problem link: https://leetcode.com/problems/search-insert-position/
+
+
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        
+        l,r=0, len(nums)-1
+        while l <= r:
+            mid = (l+r) //2
+            if target == nums[mid]:
+                return mid
+            if target > nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return l
