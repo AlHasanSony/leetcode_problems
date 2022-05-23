68da76fe4146c0b87af27d21c9185dd13d5af650
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-05-23 22:44:54 +0600

    updated 23_5_22

--- python/283.move_zeroes_leetcode.py
+++ python/283.move_zeroes_leetcode.py
@@ -22,6 +22,20 @@
 # -231 <= nums[i] <= 231 - 1
 
 
+class Solution:
+    def moveZeroes(self, nums): 
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, 0 # l - left pointer, r - right pointer
 
+        while True: # loop until all zeroes are moved to the end
+            while r < len(nums) and nums[r] != 0: r += 1 # move right pointer to the first zero
+            while l < len(nums) and (nums[l] == 0 or l < r): l += 1 # move left pointer to the first non-zero element
+            if l == len(nums) or r == len(nums): break # if all elements are zeroes, break the loop
+            nums[l], nums[r] = nums[r], nums[l] # swap the elements
+            l += 1 # move left pointer to the next non-zero element
+            r += 1 # move right pointer to the next zero element
 
 
