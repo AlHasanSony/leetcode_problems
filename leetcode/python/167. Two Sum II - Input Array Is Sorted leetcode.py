0634c26f66af73511a9dd9470938968b2724f17e
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-05-31 21:34:00 +0600

    leetcode

--- leetcode/python/167. Two Sum II - Input Array Is Sorted leetcode.py
+++ leetcode/python/167. Two Sum II - Input Array Is Sorted leetcode.py
@@ -6,6 +6,7 @@ class Solution:
         
         left = 0
         right = len(numbers) - 1
+        
         while left < right:
             if numbers[left] + numbers[right] == target:
                 return [left + 1, right + 1]
