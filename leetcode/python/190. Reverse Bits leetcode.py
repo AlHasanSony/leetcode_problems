041bf110e121bcad237f1958eb350dc5410f091e
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-06-02 02:15:35 +0600

    leetcode

--- leetcode/python/190. Reverse Bits leetcode.py
+++ leetcode/python/190. Reverse Bits leetcode.py
@@ -6,7 +6,8 @@ class Solution:
     # @return an integer
     def reverseBits(self, n: int) -> int:
         num = 0
-        for i in range(32):
-            if (n >> i) & 1:
+               
+        for i in range(32): 
+            if (n >> i) & 1: # if the bit is 1 and the bit is in the rightmost position
                 num += ((n >> i) & 1) << (31 - i)
         return num
