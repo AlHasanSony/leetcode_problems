293212d6e7e227a2f63e6e4091174ee40c1eec9c
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-06-20 16:09:21 +0600

    leetcode

---
+++ leetcode/python/15. 3Sum leetcode.py
@@ -0,0 +1,17 @@
+
+#problem link - https://leetcode.com/problems/3sum/
+
+class Solution:
+    def threeSum(self, nums:list[int]) -> list[list[int]]:
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not len(nums):
+            return None
+        ret = set()
+        nums.sort() #O(Nlog(N))
+        for idx, n in enumerate(nums): #O(N)
+            for two in self.twoSum(nums[idx + 1:], -n): #O(N)
+                    ret.add(tuple([n] + two)) #O(1)
+        return list(ret) #O(N)
