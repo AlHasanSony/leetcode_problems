afdc83e117b34f18c92c700e17e5b1e35f3d1dd7
Al Hasan Sony <sonyhasan.git@gmail.com>
2022-06-25 10:44:24 +0600

    leetcode

--- leetcode/python/1137. N-th Tribonacci Number leetcode.py
+++ leetcode/python/1137. N-th Tribonacci Number leetcode.py
@@ -1,33 +1,18 @@
 #problem link - https://leetcode.com/problems/n-th-tribonacci-number/
 
+
 class Solution:
     def tribonacci(self, n: int) -> int:
         if n <= 1:
             return n
         elif n == 2:
             return 1
-        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-    
-
-
-
-
-
-
-
-
-# class Solution:
-#     def tribonacci(self, n: int) -> int:
-#         if n <= 1:
-#             return n
-#         elif n == 2:
-#             return 1
         
-#         first, second, third = 0, 1, 1
-#         for _ in range(3, n+1):
-#             first, second, third = second, third, first + second + third
+        first, second, third = 0, 1, 1
+        for _ in range(3, n+1):
+            first, second, third = second, third, first + second + third
         
-#         return third
+        return third
 
 
 # class Solution1:
@@ -39,4 +24,16 @@ class Solution:
 #             memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo) + self.helper(n-3, memo)
 #         return memo[n]
 
-        
+
+
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         if n <= 1:
+#             return n
+#         elif n == 2:
+#             return 1
+#         return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+    
+
+
+
